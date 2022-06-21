65d3cf8ad603376af49f188665b8eddbd59f393a
Ankit <ankit@Ankits-MacBook-Air.local>
2022-06-21 13:59:54 +0530

    basic error handling on API routes

--- routes/api/api.routes.js
+++ routes/api/api.routes.js
@@ -9,24 +9,32 @@ const axios = require('axios')
 // response data : [ {...data}, {}, {}, ...] array of objects
 router.get('/top/artists', isLoggedIn, async (req, res) => {
     const token = `Bearer ${req.user.token}`
-    const response = await axios.get("https://api.spotify.com/v1/me/top/artists", {
-        headers: {
-            'Authorization': token
-        }
-    })
-    res.send(response.data.items)
+    try {
+        const response = await axios.get("https://api.spotify.com/v1/me/top/artists", {
+            headers: {
+                'Authorization': token
+            }
+        })
+        res.send(response.data.items)
+    } catch (error) {
+        res.status(403).send(error.message)
+    }
 })
 
 // Get top user's tracks 
 // response data : [ {...data}, {}, {}, ...] array of objects
 router.get('/top/tracks', isLoggedIn, async (req, res) => {
     const token = `Bearer ${req.user.token}`
-    const response = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
-        headers: {
-            'Authorization': token
-        }
-    })
-    res.send(response.data.items)
+    try {
+        const response = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
+            headers: {
+                'Authorization': token
+            }
+        })
+        res.send(response.data.items)
+    } catch (error) {
+        res.status(403).send(error.message)
+    }
 })
 
 // Get top spotify recommendations 
