7dff894fb8b1686884a4ea77c1f07ad56c25ba1b
Ankit <ankit@Ankits-MacBook-Air.local>
2022-06-27 18:06:48 +0530

    response data cleanup

--- utils/api.utils.js
+++ utils/api.utils.js
@@ -1,18 +1,18 @@
+// Cleans response data
 const topArtistsDataCleanup = (full_data) => {
     const allowed_data = ['href', 'id', 'images', 'name',]
     const send_data = []
     for (let i = 0; i < full_data.length; i++) {
         const data_object = {}
         allowed_data.map(key => {
-
             data_object[key] = full_data[i][key]
-
         })
         send_data[i] = data_object
     }
     return send_data
 }
 
+// Cleans response data
 const topTracksDataCleanup = (full_data) => {
     const allowed_data = ['external_urls', 'href', 'id', 'name',]
     const send_data = []
