cf858a1723cbe3b556164719c3c6cc055bd84ce2
Ankit <ankit@Ankits-MacBook-Air.local>
2022-06-27 18:09:15 +0530

    response data cleanup

--- routes/api/api.routes.js
+++ routes/api/api.routes.js
@@ -39,18 +39,4 @@ router.get('/top/tracks', isLoggedIn, async (req, res) => {
     }
 })
 
-// Get top spotify recommendations 
-// response data : [ {...data}, {}, {}, ...] array of objects
-// router.get('/recommendations', isLoggedIn, async (req, res) => {
-//     const token = `Bearer ${req.user.token}`
-//     const response = await axios.get("https://api.spotify.com/v1/recommendations", {
-//         headers: {
-//             'Authorization': token
-//         }
-//     })
-//     res.send(response.data.items)
-// })
-
-
-
 module.exports = router
